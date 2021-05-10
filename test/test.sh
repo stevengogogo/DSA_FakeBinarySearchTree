d221393111460b52a35c247f2152f811c3f27e36
stevengogogo <stevengogogo4321@gmail.com>
2021-05-11 02:22:04 +0800

    add data

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@
 echo ""
 echo "$(tput setaf 2; tput bold)Start test: Input/Output $(tput sgr0)"
 
-for i in {1..3};
+for i in {1..4};
 do
     echo ""
     echo "$(tput setaf 5)Test File: $i.in$(tput sgr0)";
