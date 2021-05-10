78521400f7041436a471f4b2306a74de0dd4ecaa
stevengogogo <stevengogogo4321@gmail.com>
2021-05-11 00:41:47 +0800

    Clear legacy code

--- src/utils.h
+++ src/utils.h
@@ -11,25 +11,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-/**
- * @brief Store extreme value
- * @param extreme extreme value
- * @param arg argument
- */
-typedef struct {
-    int extreme;
-    int arg;
-} extm;
 
-/**
- * @brief 
- * 
- * @param a 
- * @param b 
- * @return extm 
- */
-extm max(int* a,int* b);
-extm min(int*,int*);
 
 
 
